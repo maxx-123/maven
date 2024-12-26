899bfe756c95d35a42b0ef6e5f30dca524d917b0
USER <USER@DESKTOP-IN41SNK>
2024-12-26 09:24:24 +0530

    maven project saved

---
+++ maven/src/main/java/com/crm/pom/CampaignPage.java
@@ -0,0 +1,26 @@
+package com.crm.pom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CampaignPage {
+	public void CreateCampaign() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("http://192.168.61.129:8080/crm/HomePage.do");
+		driver.findElement(By.id("username")).sendKeys("rashmi@dell.com");
+		driver.findElement(By.id("password")).sendKeys("123456"+Keys.ENTER);
+		
+		
+		driver.quit();
+		
+		
+		
+	}
+
+}
